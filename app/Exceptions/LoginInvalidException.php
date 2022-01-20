918365f7fdaec14c6cf6a3dc08b7866f93608fba
rodrigo <rodrigo.goncalves2001r@gmail.com>
2022-01-20 17:29:36 -0300

    add: file LoginException

---
+++ app/Exceptions/LoginInvalidException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+class LoginInvalidException extends Exception
+{
+    protected $message = 'Email and password don\'t match.';
+
+    public function render(): JsonResponse
+    {
+        return response()->json([
+            'error' => class_basename($this),
+            'message' => $this->getMessage()
+        ]);
+    }
+}
