6d3f7465a065bca0cfe7cfd5d3d2963dd8aa1bd6
rodrigo <rodrigo.goncalves2001r@gmail.com>
2022-01-20 17:32:33 -0300

    add: method login implemented

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\LoginInvalidException;
+use App\Http\Requests\AuthLoginRequest;
+use App\Http\Resources\UserResource;
+use App\Services\AuthService;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
 class AuthController extends Controller
 {
-    public function login()
+    private $authService;
+
+    public function __construct(AuthService $authService)
     {
-        dd('x');
+        $this->authService = $authService;
+    }
+
+    /**
+     * @throws LoginInvalidException
+     */
+    public function login(AuthLoginRequest $request): UserResource
+    {
+        $input = $request->validated();
+        $token = $this->authService->login($input['email'], $input['password']);
+
+        return (new UserResource(auth()->user()))->additional($token);
     }
 }
