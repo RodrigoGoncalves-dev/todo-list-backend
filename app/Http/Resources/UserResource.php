ee40edee8702ee24a02824ff8ac6c749c1191216
rodrigo <rodrigo.goncalves2001r@gmail.com>
2022-01-20 17:36:16 -0300

    add: UserResources for improve response

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => (integer)$this->id,
+            'first_name' => (string)$this->first_name,
+            'last_name' => (string)$this->last_name,
+            'email' => (string)$this->email,
+            'created_at' => (string)$this->created_at
+        ];
+    }
+}
