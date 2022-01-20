823e1519dfeefe0480072b717732c500ac29f125
rodrigo <rodrigo.goncalves2001r@gmail.com>
2022-01-20 17:39:13 -0300

    add: AuthService returns token

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -1,6 +1,30 @@
 <?php
 
+namespace App\Services;
+
+use App\Exceptions\LoginInvalidException;
+
 class AuthService
 {
+    /**
+     * @throws LoginInvalidException
+     */
+    public function login(string $email, string $password)
+    {
+        $login = [
+            'email' => $email,
+            'password' => $password,
+        ];
 
+        $token = auth()->attempt($login);
+
+        if(!$token) {
+            throw new LoginInvalidException();
+        }
+
+        return [
+            'access_token' => $token,
+            'token_type' => 'Bearer'
+        ];
+    }
 }
