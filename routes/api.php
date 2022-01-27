c1324636d3b6ec7dfd4b130f4148d594057c08fe
rodrigo <rodrigo.goncalves2001r@gmail.com>
2022-01-26 22:54:13 -0300

    added route register

--- routes/api.php
+++ routes/api.php
@@ -6,5 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
 });
 
